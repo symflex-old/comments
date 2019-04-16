db19d9c25114b6c70bf83dc1ab32f3cf02b69178
Aleksandr <ak@symflex.ru>
2019-04-16 22:25:30 +0500

    save

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ $(function () {
         }
 
         for (let comment of comments) {
-            tree += '<li data-comment-id="'+comment.id+'">';
+            tree += '<li data-comment-id="' + comment.id + '">';
             tree += '<div class="comment">';
             tree += '<span>Дата: ';
             tree += comment.date;
@@ -35,7 +35,7 @@ $(function () {
             tree += '<div class="text">';
             tree += comment.text
             tree += '</div>';
-            tree += '<div class="reply" data-reply-post-id="'+comment.post_id+'" data-reply-comment-id="'+comment.id+'">Ответить</div>';
+            tree += '<div class="reply" data-reply-post-id="' + comment.post_id + '" data-reply-comment-id="' + comment.id + '">Ответить</div>';
             tree += '</div>';
             if (comment.hasOwnProperty('children')) {
                 tree += createTreeComments(comment.children, true);
@@ -53,43 +53,44 @@ $(function () {
         $(this).hide();
         let commentId = $(this).data('reply-comment-id');
 
-        let form = '<div data-form-reply-id="'+commentId+'">';
-            form += '<div class="form-group">';
-            form += '<textarea class="form-control" required></textarea>';
-            form += '</div>';
-            form += '<div class="group-button">';
-            form += '<div class="btn btn-primary btn-sm cancel-reply" data-id="'+commentId+'">Отмена</div>';
-            form += '<div class="btn btn-success btn-sm send-reply" data-id="'+commentId+'">Отправить</div>';
-            form += '</div>';
-            form += '</div>';
-        ;
+        let form = '<div data-form-reply-id="' + commentId + '">';
+        form += '<div class="form-group">';
+        form += '<textarea class="form-control" required></textarea>';
+        form += '</div>';
+        form += '<div class="group-button">';
+        form += '<div class="btn btn-primary btn-sm cancel-reply" data-id="' + commentId + '">Отмена</div>';
+        form += '<div class="btn btn-success btn-sm send-reply" data-id="' + commentId + '">Отправить</div>';
+        form += '</div>';
+        form += '</div>';
+
         $(this).after(form)
     })
 
     // скрыть форму ответа на комментарий
     $('.comments').on('click', '.cancel-reply', function (e) {
         let id = $(this).data('id');
-        $('.comments').find('[data-reply-comment-id="'+id+'"]').show();
-        $('.comments').find('[data-form-reply-id="'+id+'"]').remove();
+        $('.comments').find('[data-reply-comment-id="' + id + '"]').show();
+        $('.comments').find('[data-form-reply-id="' + id + '"]').remove();
     })
 
     // данные из формы ответа на комментарий
     $('.comments').on('click', '.send-reply', function (e) {
 
         let comment_id = $(this).data('id');
-        let formEl = $('.comments').find('[data-form-reply-id="'+comment_id+'"]');
+        let formEl = $('.comments').find('[data-form-reply-id="' + comment_id + '"]');
         let commentText = $(formEl).find('textarea').val();
-            $.ajax({
-                url: createUrl,
-                dataType: 'json',
-                method: 'post',
-                data: {comment_id: comment_id, text: commentText}
-            }).done(function (response) {
-                //что бы не лепить код для вставки комментария в верстку просто делаем запрос и обновляем.
-                updateComments();
-                $(formEl).remove();
-                $('.comments').find('[data-reply-comment-id="'+comment_id+'"]').show();
-            });
+
+        $.ajax({
+            url: createUrl,
+            dataType: 'json',
+            method: 'post',
+            data: {comment_id: comment_id, text: commentText}
+        }).done(function (response) {
+            //что бы не лепить код для вставки комментария в верстку просто делаем запрос и обновляем.
+            updateComments();
+            $(formEl).remove();
+            $('.comments').find('[data-reply-comment-id="' + comment_id + '"]').show();
+        });
     })
 
     // данные из формы в верху страницы
